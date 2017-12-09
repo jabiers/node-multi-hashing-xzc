c5107c95e1a10e6180fab9cfb0a756d507229ec3
splxsg <splxsg@gmail.com>
2017-12-09 23:12:55 +0000

    v

--- lbry.c
+++ lbry.c
@@ -15,33 +15,42 @@ typedef struct {
 
 void lbry_hash(const void* input,void* output)
 {
-    uint32_t hashA[16], hashB[16], hashC[16];
-    lbryhash_context_holder ctx;
-    memset(hashA, 0, 16 * sizeof(uint32_t));
-    memset(hashB, 0, 16 * sizeof(uint32_t));
-    memset(hashC, 0, 16 * sizeof(uint32_t));
+   sph_sha256_context      ctx_sha256 __attribute__ ((aligned (64)));
+   sph_sha512_context      ctx_sha512 __attribute__ ((aligned (64)));
+   sph_ripemd160_context   ctx_ripemd __attribute__ ((aligned (64)));
+   uint32_t _ALIGN(64) hashA[16];
+   uint32_t _ALIGN(64) hashB[16];
+   uint32_t _ALIGN(64) hashC[16];
 
-    sph_sha256 (&ctx.sha256, input, 112);
-    sph_sha256_close(&ctx.sha256, hashA);
+   sph_sha256_init( &ctx_sha256 );
+   sph_sha256 ( &ctx_sha256, input, 112 );
+   sph_sha256_close( &ctx_sha256, hashA );
 
-    sph_sha256 (&ctx.sha256, hashA, 32);
-    sph_sha256_close(&ctx.sha256, hashA);
+   sph_sha256_init( &ctx_sha256 );
+   sph_sha256 ( &ctx_sha256, hashA, 32 );
+   sph_sha256_close( &ctx_sha256, hashA );
 
-    sph_sha512 (&ctx.sha512, hashA, 32);
-    sph_sha512_close(&ctx.sha512, hashA);
+   sph_sha512_init( &ctx_sha512 );
+   sph_sha512 ( &ctx_sha512, hashA, 32 );
+   sph_sha512_close( &ctx_sha512, hashA );  
 
-    sph_ripemd160 (&ctx.ripemd, hashA, 32);
-    sph_ripemd160_close(&ctx.ripemd, hashB);
+   sph_ripemd160_init( &ctx_ripemd );
+   sph_ripemd160 ( &ctx_ripemd, hashA, 32 );
+   sph_ripemd160_close( &ctx_ripemd, hashB );
 
-    sph_ripemd160 (&ctx.ripemd, hashA+8, 32);
-    sph_ripemd160_close(&ctx.ripemd, hashC);
+   sph_ripemd160_init( &ctx_ripemd );
+   sph_ripemd160 ( &ctx_ripemd, hashA+8, 32 );
+   sph_ripemd160_close( &ctx_ripemd, hashC );
 
-    sph_sha256 (&ctx.sha256, hashB, 20);
-    sph_sha256 (&ctx.sha256, hashC, 20);
-    sph_sha256_close(&ctx.sha256, hashA);
+   sph_sha256_init( &ctx_sha256 );
+   sph_sha256 ( &ctx_sha256, hashB, 20 );
+   sph_sha256 ( &ctx_sha256, hashC, 20 );
+   sph_sha256_close( &ctx_sha256, hashA );
 
-    sph_sha256 (&ctx.sha256, hashA, 32);
-    sph_sha256_close(&ctx.sha256, hashA);
+   sph_sha256_init( &ctx_sha256 );
+   sph_sha256 ( &ctx_sha256, hashA, 32 );
+   sph_sha256_close( &ctx_sha256, hashA );
 
-    memcpy(output, hashA, 32);
+   memcpy( output, hashA, 32 );
+
 }
