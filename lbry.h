38606facfe6acc3779e6c9bde1fbcc58c87acd21
splxsg <splxsg@gmail.com>
2017-12-09 15:58:50 +0000

    v

--- lbry.h
+++ lbry.h
@@ -11,37 +11,34 @@
 extern "C" {
 #endif 
 
-#include "lbry/ripemd160.h"
-#include "lbry/sha256.h"
-//#include "lbry/uint256.h"
-#include "lbry/sha512.h"
-#include "lbry/common.h"
+//#include "lbry/ripemd160.h"
+//#include "lbry/sha256.h"
+//#include "lbry/sha512.h"
+//#include "lbry/common.h"
 
-//typedef uint256 ChainCode;
-void lbry_hash(char* input, char* output);
-/** A hasher class for Bitcoin's 256-bit hash (double SHA-256). */
-class CHash256 {
-private:
-    CSHA256 sha;
-public:
-    static const size_t OUTPUT_SIZE = CSHA256::OUTPUT_SIZE;
+// void lbry_hash(char* input, char* output);
+// class CHash256 {
+// private:
+//     CSHA256 sha;
+// public:
+//     static const size_t OUTPUT_SIZE = CSHA256::OUTPUT_SIZE;
 
-    void Finalize(unsigned char hash[OUTPUT_SIZE]) {
-        unsigned char buf[sha.OUTPUT_SIZE];
-        sha.Finalize(buf);
-        sha.Reset().Write(buf, sha.OUTPUT_SIZE).Finalize(hash);
-    }
+//     void Finalize(unsigned char hash[OUTPUT_SIZE]) {
+//         unsigned char buf[sha.OUTPUT_SIZE];
+//         sha.Finalize(buf);
+//         sha.Reset().Write(buf, sha.OUTPUT_SIZE).Finalize(hash);
+//     }
 
-    CHash256& Write(const unsigned char *data, size_t len) {
-        sha.Write(data, len);
-        return *this;
-    }
+//     CHash256& Write(const unsigned char *data, size_t len) {
+//         sha.Write(data, len);
+//         return *this;
+//     }
 
-    CHash256& Reset() {
-        sha.Reset();
-        return *this;
-    }
-};
+//     CHash256& Reset() {
+//         sha.Reset();
+//         return *this;
+//     }
+// };
 
 
 #endif
