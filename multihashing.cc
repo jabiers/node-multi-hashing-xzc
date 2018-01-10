1380f37de2528f5cfcf13395ddddfc1670520538
Daehyun Kim <jabiers87@gmail.com>
2018-01-11 00:51:11 +0900

    Update multihashing.cc

--- multihashing.cc
+++ multihashing.cc
@@ -2,8 +2,7 @@
 #include <node_buffer.h>
 #include <v8.h>
 #include <stdint.h>
-#include <nan.h>
-#include <stdio.h>
+#include "nan.h"
 
 extern "C" {
     #include "bcrypt.h"
@@ -31,8 +30,7 @@ extern "C" {
     #include "lbry.h"
 }
 
-#define THROW_ERROR_EXCEPTION(x) NanThrowError(x)
-#define THROW_ERROR_EXCEPTION_WITH_STATUS_CODE(x, y) NanThrowError(x, y)
+#define THROW_ERROR_EXCEPTION(x) Nan::ThrowError(x)
 
 using namespace node;
 using namespace v8;
@@ -593,23 +591,22 @@ NAN_METHOD(lyra2z) {
 }
 
 NAN_METHOD(lbry) {
-    NanScope();
-
-    if (args.Length() < 1)
+    if (info.Length() < 1)
         return THROW_ERROR_EXCEPTION("You must provide one argument.");
 
-    Local<Object> target = args[0]->ToObject();
+    Local<Object> target = Nan::To<Object>(info[0]).ToLocalChecked();
 
     if(!Buffer::HasInstance(target))
         return THROW_ERROR_EXCEPTION("Argument should be a buffer object.");
 
     char * input = Buffer::Data(target);
-    char output[32];
-    lbry_hash(input, output);
+    char *output = (char*) malloc(sizeof(char) * 32);
 
-    NanReturnValue(
-        NanNewBufferHandle(output, 32)
-    );
+    uint32_t input_len = Buffer::Length(target);
+
+    lbry_hash(input, output, input_len);
+
+    info.GetReturnValue().Set(Nan::NewBuffer(output, 32).ToLocalChecked());
 }
 
 
